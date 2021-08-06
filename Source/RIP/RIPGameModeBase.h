5ef0d46af1abd1de7bfbd4b1b9773bc1d5c2ebac
leonslhu <leonslhu@tencent.com>
2021-08-06 21:41:49 +0800

    init project.

---
+++ Source/RIP/RIPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RIPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIP_API ARIPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
